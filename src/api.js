33604b0fb68373c71e32fd63d260d484647ca1df
Kasra Rahjerdi <hi@kasra.codes>
2016-09-05 18:23:49 -0700

    Tiny tiny API refactor to save my head later

--- src/api.js
+++ src/api.js
@@ -1,6 +1,9 @@
 export default class ArenaAPI {
-  getChannel(channelName) {
-    return fetch('https://arena-api.herokuapp.com/v2/channels/communist-memes')
+  constructor() {
+    this.BASE_URL = 'https://arena-api.herokuapp.com/v2/';
+  }
+  getChannel(channel_name) {
+    return fetch(this.BASE_URL + 'channels/' + channel_name)
       .then((response) => response.json())
       .catch((error) => {
           console.error(error);
